c2fb2707b8b1c34a97b1b245e50bd0c5ef916e0b
MatheusNunes166 <matheus06nunes@hotmail.com>
2023-07-27 15:12:22 -0300

    commit inicial

---
+++ Presenca/src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
@@ -0,0 +1,17 @@
+package com.MundoSenai.Presenca.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class C_Pessoa {
+    @GetMapping("/")
+    public String helloworld(){
+        return "Login/login";
+    }
+
+    @GetMapping("/teste")
+    public String teste(){
+        return "teste";
+    }
+}
